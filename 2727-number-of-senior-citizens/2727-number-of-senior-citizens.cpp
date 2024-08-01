df4bee22cd06ad7a9755a7e67876a9e875e6e36f
Dolly Yadav <100135192+LearnInspire@users.noreply.github.com>
2024-08-01 12:44:07 +0530

    Time: 12 ms (13.14%), Space: 17.6 MB (27.35%) - LeetHub

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     int countSeniors(vector<string>& details) {
-        int count = 0;
-
-        //iterate through the string
-        for(int i = 0; i<details.size(); i++)
-        {
-            if(details[i][11] > '6' || details[i][11] == '6' && details[i][12] > '0')
-            count++;
+        //converting char to string first & then calculate age by getting
+        // ones & tens places and checking if it's graeter than 60 or not
+        int ans = 0;
+        for(auto i:details){
+            int age = (i[11] - '0')*10 + (i[12] - '0');
+            if(age > 60) ans++;
         }
-        return count;
+        return ans;
+
+        /*
+        int count = 0;
+        // iterate through the array of strings
+        for (int i = 0; i < details.size(); i++) {
+            if (details[i][11] > '6' ||
+                details[i][11] == '6' && details[i][12] > '0')
+                count++;
+        }
+        return count; */
     }
 };
