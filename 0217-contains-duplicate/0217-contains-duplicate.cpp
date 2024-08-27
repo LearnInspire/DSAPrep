295c7e09af8a6e3a8174b3f0c54545e282bdc6c7
Dolly Yadav <100135192+LearnInspire@users.noreply.github.com>
2024-08-27 22:24:08 +0530

    Time: 93 ms (55.05%), Space: 73.5 MB (42.71%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
-//Brute Force -> is to iterate through the array & compare very ele with every other ele, TC: O(n2), SC: O(1)
-//Better -> sort the array first, while traversing in the first pass have two pointers, compare both of the elements
+//Brute Force -> is to iterate through the array & compare every ele with every other ele, TC: O(n2), SC: O(1)
+//2) Better -> sort the array first, while traversing in the first pass have two pointers, compare both of the elements
 // & increment the pointers after each comparison. TC: O(n logn), sc: O(n)
 //Optimal --> create one hashsset bcz set doesn't contains duplicates, start iterating the array if element isn't presnt in set
 //i.e we haven't find the second instance yet & add the number to the set. If it's already present then return true.
 // If there are no duplicates found then return false.
-    bool containsDuplicate(vector<int>& nums) {
+/*A set is chosen because it provides average (O(1)) time complexity for both insertions and membership checks, 
+which makes it efficient for checking duplicates.*/
+
+/*Sol 2:    bool containsDuplicate(vector<int>& nums) {
         
         sort(nums.begin(), nums.end());
 
@@ -16,5 +19,16 @@ public:
             }
         }
         return false;
+    }*/
+
+/*Sol 3:*/  bool containsDuplicate(vector<int>& nums) {
+    unordered_set <int> seen;
+
+    for(int i:nums){
+        if(seen.count(i)>0)
+            return true;
+        seen.insert(i);
+    }
+    return false;
     }
 };
