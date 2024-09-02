cfa7ca88ea79724e9df015047bbaee0283f948c7
Dolly Yadav <100135192+LearnInspire@users.noreply.github.com>
2024-09-02 19:55:58 +0530

    Time: 79 ms (67.7%), Space: 88.1 MB (73.82%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    vector<vector<int>> construct2DArray(vector<int>& o, int m, int n) {
-        int i = 0, j = 0;
-        vector<vector<int>>res(m, vector<int>(n));
-        int s = o.size();
-        if(s != m*n) return {};
-        for(int k = 0; k < s; k++) {
-            if(j%n == 0 && j != 0) {
-                i++;
-                j = 0;
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        //using nested loops, TC: O(m*n) or O(len)
+        int len = original.size();
+        if(len != m*n) return {};
+
+        int x = 0; //pointer for 1d array
+        vector<vector<int>> res(m, vector<int>(n));
+
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                res[i][j] = original[x++];
             }
-            res[i][j] = o[k];
-            j++;
         }
         return res;
-        
     }
 };
