5a8d37f7402f27ec8043c80f5ea5b351fed847b7
Dolly Yadav <100135192+LearnInspire@users.noreply.github.com>
2024-09-08 00:12:24 +0530

    Time: 30 ms (24.71%), Space: 30.8 MB (74.94%) - LeetHub

--- 1484-linked-list-in-binary-tree/1484-linked-list-in-binary-tree.cpp
+++ 1484-linked-list-in-binary-tree/1484-linked-list-in-binary-tree.cpp
@@ -21,24 +21,22 @@
  */
 class Solution {
 public:
-    bool check(ListNode* head, TreeNode* root) {
-        if(!head) {
-            return true;
-        }
+    bool check(ListNode* head, TreeNode* root){
+        if(head == NULL) return true;
+        if(root == NULL) return false;
+        if(head-> val != root->val) return false;
 
-        if(!root || root->val != head->val) {
-            return false;
-        }
-
-        return check(head->next, root->left) || check(head->next, root->right);
+        return check(head->next, root->left)  ||
+               check(head->next, root->right);
     }
 
+
+
     bool isSubPath(ListNode* head, TreeNode* root) {
-        if(!root) {
-            return false;
-        }
-        return check(head, root)        || 
-               isSubPath(head, root->left)  || 
-               isSubPath(head, root->right);
+        if(root == NULL) return false;
+
+        return check(head, root) ||
+            isSubPath(head, root->left) ||
+            isSubPath(head, root->right);
     }
 };
