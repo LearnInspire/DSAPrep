c1c217b1299b99a032a7963dd20febbc3988bf68
Dolly Yadav <100135192+LearnInspire@users.noreply.github.com>
2024-09-11 16:03:39 +0530

    Time: 0 ms (100%), Space: 7.7 MB (44.87%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
+/*Bit counting: Use a loop to repeatedly check the last bit of the XOR result using & 1. 
+Right shift the XOR result (>> 1) to process the next bit. Continue until all bits are processed. 
+The TC is O(k), where k is the number of bits in the binary representation of the larger of the two numbers (start or goal).*/
     int minBitFlips(int start, int goal) {
         int count = 0;
+        // XOR will give 1 where the bits are different
         int differentBit = start ^ goal;
         while(differentBit){
-            //AND operation with 1
-            if(differentBit & 1){
-                count ++;
-            }
-            //right shift by 1
+            //If the last bit is 1, increment count
+            count += differentBit & 1;
+            //Right-shift to check the next bit
             differentBit = differentBit >> 1;
         }
         return count;
