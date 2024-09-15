7d5496de69c276609198afe70cec010dfa958be0
Dolly Yadav <100135192+LearnInspire@users.noreply.github.com>
2024-09-15 21:49:40 +0530

    Time: 79 ms (46.74%), Space: 18.9 MB (51.86%) - LeetHub

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -1,6 +1,44 @@
 class Solution {
 public:
+    /*A2: we don't need any array to store freq of vowels, we can take a mask & do the XOR with 1<<x, x = no of shifts
+    as per the vowel, for a do shift 1 time, for e do it 2 times, for i for 3 times & so on*/
     int findTheLongestSubstring(string s) {
+        int mask = 0; //00000 presenting aeiou frequencies
+        unordered_map<int, int> mp;
+        mp[mask] = -1; //initially
+        int result = 0;
+
+        for(int i = 0; i < s.length(); ++i){
+            if(s[i] == 'a'){
+                mask = mask ^ (1<<0);
+            }
+            if(s[i] == 'e'){
+                mask = mask ^ (1<<1);
+            }
+            if(s[i] == 'i'){
+                mask = mask ^ (1<<2);
+            }
+            if(s[i] == 'o'){
+                mask = mask ^ (1<<3);
+            }
+            if(s[i] == 'u'){
+                mask = mask ^ (1<<4);
+            }
+
+            if(mp.find(mask) != mp.end()){
+                result = max(result, i - mp[mask]);
+            }
+            else{
+                mp[mask] = i;
+            }
+        }
+        return result;
+    }
+
+
+
+
+    /*int findTheLongestSubstring(string s) {
         //A1: storing freq of vowels in array of size 5 then storing the array state in strings,
         //storing the states in map & do look up in map & find the length of the substr, TC: O(n)
         unordered_map<string, int> mp;
@@ -44,5 +82,5 @@ public:
         }
         }
         return result;
-    }
+    }*/
 };
