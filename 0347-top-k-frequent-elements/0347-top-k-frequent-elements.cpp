cfcd66b34cb484f417d38826e272cf3959a47aee
Dolly Yadav <100135192+LearnInspire@users.noreply.github.com>
2024-09-25 04:47:20 +0530

    Time: 8 ms (88.2%), Space: 17.5 MB (57.61%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,4 +1,38 @@
+//Approach-1 (Using min-heap) - TC : O(nlog(n-k))
 class Solution {
+public:
+    typedef pair<int, int> p;
+    
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+        //min-heap
+        priority_queue<p, vector<p>, greater<p>> pq;
+        
+        //count frequency of each element
+        // Worst Case - n distinct elements are stored, so, space O(n)
+        unordered_map<int, int> mp;
+        for(int i : nums)
+            mp[i]++;
+        
+        //Push in min-heap and maintain size k
+        for(auto it:mp) {
+            pq.push({it.second, it.first});
+            
+            if(pq.size() > k)
+                pq.pop();
+        }
+        
+        
+        //Pick all top K elements
+        vector<int> result;
+        while(!pq.empty()) {
+            result.push_back(pq.top().second);
+            pq.pop();
+        }
+        return result;
+    }
+};
+/*class Solution {
 public:
     //Approach-2 (Using Bucket Sort) - TC : O(n) - We visit all elements of nums only once
     vector<int> topKFrequent(vector<int>& nums, int k) {
@@ -31,3 +65,4 @@ public:
         return result;
     }
 };
+*/
