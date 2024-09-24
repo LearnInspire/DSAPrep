726eff5c366a2bdbdab584e4957a9431762002c8
Dolly Yadav <100135192+LearnInspire@users.noreply.github.com>
2024-09-25 03:06:44 +0530

    Time: 11 ms (68.39%), Space: 19.3 MB (5.94%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    //Approach-2 (Using Bucket Sort) - TC : O(n) - We visit all elements of nums only once
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        //store all the freq in map
+        unordered_map<int, int> mp;
+        for(auto &num: nums){
+            mp[num]++;
+        }
+
+        //define one bucket array where freq is arr[i] & index is element, index is freq, value = elements
+        //bucket[i] = elements having i frequency
+        int n = nums.size();
+        vector<vector<int>> bucket(n+1); //n+1 bcz of size of array
+        for(auto &it: mp){
+            int element = it.first;
+            int freq = it.second;
+            bucket[freq].push_back(element);
+        }
+
+        //traverse the bucket array from left to right, bcz max freq will be from n to 0 & pop the top k elements
+        vector<int> result;
+        for(int i = n; i>=0; i--){
+            if(bucket[i].size() == 0) continue; //bucket khali hai, uss freq ka koi element nhi avail hai
+            while(bucket[i].size() > 0 && k > 0){
+                result.push_back(bucket[i].back()); //back operation takes less time
+                bucket[i].pop_back();
+                k--;
+            }
+        }
+        return result;
+    }
+};
