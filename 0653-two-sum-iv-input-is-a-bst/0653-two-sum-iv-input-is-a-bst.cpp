0606b87321eb68e388c5e860288cdbdeceb966ce
Dolly Yadav <100135192+LearnInspire@users.noreply.github.com>
2024-10-01 18:03:37 +0530

    Time: 35 ms (26.32%), Space: 38.9 MB (19.24%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -11,6 +11,17 @@
  */
 class Solution {
 public:
+
+    /*Approach 2: store the values in hashmap or set & then find k - root's value in set, if there then return true*/
+    unordered_set<int> st;
+    bool findTarget(TreeNode* root, int k){
+        if(root == NULL) return false;
+        if(st.find(k-root->val) != st.end()) return true;
+        st.insert(root->val);
+        return findTarget(root->left, k) || findTarget(root->right, k);
+    }
+    
+    /* Approach1: storing bst in array & applying 2 pointer on array just like 2 sum, TC:O(n) SC:O(n)
     vector<int> array;
 
     void inorder(TreeNode* root){
@@ -32,5 +43,5 @@ public:
             else right--;
         }
         return false;
-    }
+        }*/
 };
