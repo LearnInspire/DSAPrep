b9c33ab6a4eae05fa9af88097c43f3de7060cdb0
Dolly Yadav <100135192+LearnInspire@users.noreply.github.com>
2024-10-12 01:14:47 +0530

    Time: 4 ms (32.17%), Space: 9.4 MB (63.17%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    int choose(int ind, vector<int>& nums, vector<int>& dp){
-        //base case
-        if(ind == 0) return nums[ind];
-        if(ind < 0) return 0;
-        if(dp[ind] != -1) return dp[ind];
+    int choose(int n, vector<int>& nums, vector<int>& dp) {
+        // base case
+        dp[0] = nums[0];
 
-        int pick = nums[ind] + choose(ind-2, nums, dp);
-        int nonpick = 0 + choose(ind-1, nums, dp);
-        return dp[ind] = max(pick, nonpick);
+        for (int i = 1; i < n; i++) {
+            int pick = nums[i];
+            if(i>1) pick += dp[i - 2];
+            int nonpick = 0 + dp[i - 1];
+            dp[i] = max(pick, nonpick);
+        }
+        return dp[n-1];
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n, -1);
-        return choose(n-1, nums, dp); 
+        return choose(n, nums, dp);
     }
 };
