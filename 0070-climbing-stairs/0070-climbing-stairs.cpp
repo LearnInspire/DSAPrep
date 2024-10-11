da9ad376387c131e5221c6d63ba9514efea924e7
Dolly Yadav <100135192+LearnInspire@users.noreply.github.com>
2024-10-11 10:45:40 +0530

    Time: 0 ms (100%), Space: 7.4 MB (68.02%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,47 +1,35 @@
-//Recursive solution
-/*class Solution {
-public:
-    int climbStairs(int n) {
-        if (n == 0 || n == 1) {
-            return 1;
-        }
-        return climbStairs(n-1) + climbStairs(n-2);
-    }
-};*/
-
-//Tabulation Approach: Bottom up
-/*class Solution {
-public:
-    int climbStairs(int n) {
-        if (n == 0 || n == 1) {
-            return 1;
-        }
-        vector<int> dp(n+1, -1);
-        //base cases: as there's only 1 way to return from 0 & 1
-        dp[0] = 1;
-        dp[1] = 1;
-        for(int i = 2; i<=n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        return dp[n]; //storing the result in last cell of the dp table
-    }
-};
-*/
-//Space optimization approach
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n == 0 || n == 1) {
+        // M3: Space optimization approach
+        if (n == 0 || n == 1)
             return 1;
-        }
-        //base cases: as there's only 1 way to return from 0 & 1
         int prev = 1;
         int prev2 = 1;
-        for(int i = 2; i<=n; i++){
-            int curr_i = prev + prev2;
+        for (int i = 2; i <= n; i++) {
+            int curr_i = prev2 + prev;
             prev2 = prev;
             prev = curr_i;
         }
-        return prev; //after iterative loop ended, we can simply returns prev as our ans
+        return prev;
+        /* M2: tabulation
+        if (n == 0 || n == 1)
+            return 1;
+        vector<int> dp(n + 1, -1);
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];*/
+
+        /*M1: using recursion gives TLE
+        if(n == 0) return 1;
+        if(n == 1) return 1;
+
+        int left = climbStairs(n-1);
+        int right = climbStairs(n-2);
+
+        return left + right;*/
     }
 };
