1dc2658c81f781f26bd5e9b18c2a83c354b0082d
Dolly Yadav <100135192+LearnInspire@users.noreply.github.com>
2024-10-17 05:56:19 +0530

    Time: 44 ms (58.38%), Space: 51.1 MB (42.83%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
+        //to keep track of the farthest index we can reach from the start
         int maxIndex = 0;
         //i is current index
         for(int i = 0; i<n; i++){
+            //If the current index is greater than maxIndex, it means the current index is not reachable
             if (i > maxIndex) return false;
             maxIndex = max(maxIndex, i + nums[i]);
         }
