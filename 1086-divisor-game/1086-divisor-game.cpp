335a0ebc6b142bfcddae5f3596beb4d0759188e8
Dolly Yadav <100135192+LearnInspire@users.noreply.github.com>
2024-10-19 00:41:38 +0530

    Time: 0 ms (100%), Space: 7.4 MB (22.88%) - LeetHub

--- 1086-divisor-game/1086-divisor-game.cpp
+++ 1086-divisor-game/1086-divisor-game.cpp
@@ -2,12 +2,18 @@ class Solution {
 public:
     bool divisorGame(int n) {
         //recursive solution
-        if(n<=1) return false;
+        /*if(n<=1) return false;
         for(int x = 1; x < n; x++){
             if(n % x == 0){
                 return !divisorGame(n-x);
             }
         }
-        return false;
+        return false;*/
+
+        //mathematical solution
+        if(n % 2 == 0){
+            return true;
+        }
+        else return false;
     }
 };
